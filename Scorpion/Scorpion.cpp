e01394d8af07a6c0205553923fa1b25070cf699a
Winspear22 <adnen.aloui2@hotmail.com>
2026-02-08 22:13:43 +0100

    SCORPION : endiannes faisable

--- Scorpion/Scorpion.cpp
+++ Scorpion/Scorpion.cpp
@@ -6,51 +6,45 @@
 /*   By: adnen <adnen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 14:50:17 by adnen             #+#    #+#             */
-/*   Updated: 2026/02/08 21:33:09 by adnen            ###   ########.fr       */
+/*   Updated: 2026/02/08 22:13:16 by adnen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Scorpion.hpp"
 
-Scorpion::Scorpion()
-{
+Scorpion::Scorpion() {
   std::cout << MAGENTA << "Scorpion constructor called" << RESET << std::endl;
 }
 
-Scorpion::~Scorpion()
-{
+Scorpion::~Scorpion() {
   std::cout << MAGENTA << "Scorpion destructor called" << RESET << std::endl;
 }
 
-Scorpion::Scorpion(const Scorpion &src)
-{
+Scorpion::Scorpion(const Scorpion &src) {
   std::cout << MAGENTA << "Scorpion copy constructor called" << RESET
             << std::endl;
   *this = src;
 }
 
-Scorpion &Scorpion::operator=(const Scorpion &src)
-{
+Scorpion &Scorpion::operator=(const Scorpion &src) {
   std::cout << MAGENTA << "Scorpion copy assignment operator called" << RESET
             << std::endl;
   (void)src;
   return *this;
 }
 
-void Scorpion::FileIdentification(const std::string &filename)
-{
+void Scorpion::FileIdentification(const std::string &filename) {
   std::ifstream file(filename, std::ios::binary);
-  if (!file.is_open())
-  {
-    std::cerr << BOLD_RED << "Error: could not open " << filename << RESET << std::endl;
+  if (!file.is_open()) {
+    std::cerr << BOLD_RED << "Error: could not open " << filename << RESET
+              << std::endl;
     return;
   }
 
   unsigned char headOfFile[12];
   file.read((char *)headOfFile, 12);
   std::string format = "Unknown";
-  if (file.gcount() < 2)
-  {
+  if (file.gcount() < 2) {
     std::cerr << RED << "File too small to be an image" << RESET << std::endl;
     return;
   }
@@ -63,18 +57,16 @@ void Scorpion::FileIdentification(const std::string &filename)
     format = "GIF";
   else if (headOfFile[0] == 0x42 && headOfFile[1] == 0x4D)
     format = "BMP";
-  else if (headOfFile[0] == 0x52 && headOfFile[1] == 0x49)
-  {
-    if (headOfFile[8] == 'W' && headOfFile[9] == 'E' && headOfFile[10] == 'B' && headOfFile[11] == 'P')
+  else if (headOfFile[0] == 0x52 && headOfFile[1] == 0x49) {
+    if (headOfFile[8] == 'W' && headOfFile[9] == 'E' && headOfFile[10] == 'B' &&
+        headOfFile[11] == 'P')
       format = "WEBP";
-    else
-	{
-      std::cout << RED << "It's not a valid image file (.wev or .avi) !" << RESET << std::endl;
+    else {
+      std::cout << RED << "It's not a valid image file (.wev or .avi) !"
+                << RESET << std::endl;
       return;
     }
-  }
-  else
-  {
+  } else {
     std::cout << RED << "It's not a valid image file !" << RESET << std::endl;
     return;
   }
@@ -85,28 +77,24 @@ void Scorpion::FileIdentification(const std::string &filename)
     this->_findExifBlock(file);
 }
 
-void Scorpion::_fileAnalysis(const std::string &fileName)
-{
+void Scorpion::_fileAnalysis(const std::string &fileName) {
   struct stat st;
-  if (stat(fileName.c_str(), &st) == 0)
-  {
+  if (stat(fileName.c_str(), &st) == 0) {
     this->_dateAnalysis(st);
     this->_weightAnalysis(st);
-  } 
-  else
-    std::cerr << BOLD_RED << "Error: could not stat " << fileName << RESET << std::endl;
+  } else
+    std::cerr << BOLD_RED << "Error: could not stat " << fileName << RESET
+              << std::endl;
 }
 
-void Scorpion::_dateAnalysis(const struct stat &st)
-{
+void Scorpion::_dateAnalysis(const struct stat &st) {
   char time_buf[20];
   strftime(time_buf, sizeof(time_buf), "%d/%m/%Y %H:%M",
            localtime(&st.st_mtime));
   std::cout << BOLD_YELLOW << "Date : " << RESET << time_buf << std::endl;
 }
 
-void Scorpion::_weightAnalysis(const struct stat &st)
-{
+void Scorpion::_weightAnalysis(const struct stat &st) {
   std::cout << BOLD_YELLOW << "Weight : " << RESET << st.st_size << " bytes"
             << std::endl;
 }
@@ -136,10 +124,8 @@ void Scorpion::_findExifBlock(std::ifstream &file)
           file.read(header, 6);
           if (std::string(header, 4) == "Exif")
 		  {
-            std::cout << BOLD_CYAN
-                      << "  [EXIF]   Found APP1 segment (Brute Force)" << RESET
-                      << std::endl;
-            // TODO: Lancer le parsing TIFF ici
+            std::cout << BOLD_CYAN << "  [EXIF]   Found APP1 segment" << RESET << std::endl;
+            this->_parseTiff(file);
             return;
           }
         }
@@ -150,41 +136,62 @@ void Scorpion::_findExifBlock(std::ifstream &file)
     }
   }
   std::cout << YELLOW << "  [EXIF]   No metadata found" << RESET << std::endl;
+}
 
-  /*
-     METHODE 2 : SAUT DE SEGMENTS (Optimisée - À décommenter pour tester)
-     Lire la taille des segments pour sauter par dessus.
+/*
+   METHODE 2 : SAUT DE SEGMENTS (Optimisée - À décommenter pour tester)
+   Lire la taille des segments pour sauter par dessus.
 
+void Scorpion::_findExifBlock_Optimized(std::ifstream &file)
+{
   file.seekg(2, std::ios::beg);
   unsigned char marker[2];
   while (file.read((char*)marker, 2))
   {
-          if (marker[0] != 0xFF) break;
+    if (marker[0] != 0xFF) break;
 
-          if (marker[1] == 0xE1)
-          {
-                  unsigned char sizeBytes[2];
-                  file.read((char*)sizeBytes, 2);
-                  unsigned char exifHeader[6];
-                  file.read((char*)exifHeader, 6);
-                  if (std::string((char*)exifHeader, 4) == "Exif")
-                  {
-                          std::cout << BOLD_CYAN << "  [EXIF]   Found APP1
-  segment (Segment Jump)" << RESET << std::endl;
-                          // TODO: _parseTiff(file);
-                          return;
-                  }
-          }
+    if (marker[1] == 0xE1)
+    {
+      unsigned char sizeBytes[2];
+      file.read((char*)sizeBytes, 2);
+      unsigned char exifHeader[6];
+      file.read((char*)exifHeader, 6);
+      if (std::string((char*)exifHeader, 4) == "Exif")
+      {
+        std::cout << BOLD_CYAN << "  [EXIF]   Found APP1 segment (Segment Jump)"
+<< RESET << std::endl; this->_parseTiff(file); return;
+      }
+    }
 
-          // Lire la taille pour sauter
-          unsigned char sizeBytes[2];
-          if (!file.read((char*)sizeBytes, 2)) break;
+    unsigned char sizeBytes[2];
+    if (!file.read((char*)sizeBytes, 2)) break;
 
-          unsigned short size = (sizeBytes[0] << 8) | sizeBytes[1];
-          file.seekg(size - 2, std::ios::cur);
+    unsigned short size = (sizeBytes[0] << 8) | sizeBytes[1];
+    file.seekg(size - 2, std::ios::cur);
 
-          if (marker[1] == 0xDA) break;
+    if (marker[1] == 0xDA) break;
   }
   std::cout << YELLOW << "  [EXIF]   No metadata found" << RESET << std::endl;
-  */
+}
+*/
+
+void Scorpion::_parseTiff(std::ifstream &file)
+{
+  unsigned char endian[2];
+  file.read((char *)endian, 2);
+
+  bool isLittleEndian = (endian[0] == 'I' && endian[1] == 'I');
+
+  if (isLittleEndian)
+    std::cout << BOLD_CYAN << "  [TIFF]   Endianness : Intel (Little Endian)" << RESET << std::endl;
+  else if (endian[0] == 'M' && endian[1] == 'M')
+    std::cout << BOLD_CYAN << "  [TIFF]   Endianness : Motorola (Big Endian)" << RESET << std::endl;
+  else
+  {
+    std::cout << BOLD_RED << "  [TIFF]   Error: Invalid TIFF Header" << RESET << std::endl;
+    return;
+  }
+
+  
+
 }
--- Scorpion/Scorpion.hpp
+++ Scorpion/Scorpion.hpp
@@ -6,32 +6,31 @@
 /*   By: adnen <adnen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 14:50:21 by adnen             #+#    #+#             */
-/*   Updated: 2026/02/08 17:52:01 by adnen            ###   ########.fr       */
+/*   Updated: 2026/02/08 21:58:34 by adnen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __SCORPION__
 #define __SCORPION__
 
-# include "../Spider/includes.hpp"
+#include "../Spider/includes.hpp"
 
 class Scorpion
 {
-    public:
-        Scorpion();
-        ~Scorpion();
-        Scorpion(const Scorpion &src);
-        Scorpion &operator=(const Scorpion &src);
+	public:
+		Scorpion();
+		~Scorpion();
+		Scorpion(const Scorpion &src);
+		Scorpion &operator=(const Scorpion &src);
 
-		void 	FileIdentification(const std::string& filename);
+		void		FileIdentification(const std::string &filename);
 
-    private:
-		void	_fileAnalysis(const std::string& fileName);
-		void	_dateAnalysis(const struct stat& st);
-		void	_weightAnalysis(const struct stat& st);
-		void	_findExifBlock(std::ifstream& file);
-
-        
+	private:
+		void		_fileAnalysis(const std::string &fileName);
+		void		_dateAnalysis(const struct stat &st);
+		void		_weightAnalysis(const struct stat &st);
+		void		_findExifBlock(std::ifstream &file);
+		void		_parseTiff(std::ifstream &file);
 };
 
 #endif
