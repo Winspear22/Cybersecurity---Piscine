70204c1c32a8a98d773bbda78fff2c9ae1408438
Winspear22 <adnen.aloui2@hotmail.com>
2026-02-08 15:47:01 +0100

    SCORPION : début du file analysis

--- Scorpion/Scorpion.cpp
+++ Scorpion/Scorpion.cpp
@@ -6,7 +6,7 @@
 /*   By: adnen <adnen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 14:50:17 by adnen             #+#    #+#             */
-/*   Updated: 2026/02/08 14:52:27 by adnen            ###   ########.fr       */
+/*   Updated: 2026/02/08 15:45:14 by adnen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,3 +31,40 @@ Scorpion &Scorpion::operator=(const Scorpion &src)
 {
 	std::cout << PURPLE << "Scorpion copy assignment operator called" << RESET << std::endl;
 }
+
+void 	Scorpion::FileAnalysis(const std::string& filename)
+{
+	std::ifstream file(filename, std::ios::binary);
+	if (!file.is_open())
+	{
+		std::cerr << BOLD_RED << "Error: could not open " << filename << RESET << std::endl;
+		return ;
+	}
+	unsigned char headOfFile[12];
+	file.read((char*)headOfFile, 12);
+	if (file.gcount() < 2)
+	{
+    	std::cerr << RED << "File too small to be an image" << RESET << std::endl;
+    	return;
+	}
+	
+	if (headOfFile[0] == 0xFF && headOfFile[1] == 0xD8)
+		std::cout << GREEN << "It's a jpeg/jpg file !" << RESET << std::endl;
+	else if (headOfFile[0] == 0x89 && headOfFile[1] == 0x50)
+		std::cout << BLUE << "It's a png file !" << RESET << std::endl;
+	else if (headOfFile[0] == 0x47 && headOfFile[1] == 0x49)
+		std::cout << YELLOW << "It's a GIF file !" << RESET << std::endl;
+	else if (headOfFile[0] == 0x42 && headOfFile[1] == 0x4D)
+		std::cout << MAGENTA << "It's a bmp file !" << RESET << std::endl;
+	else if (headOfFile[0] == 0x52 && headOfFile[1] == 0x49)
+	{
+		if (headOfFile[8] == 'W' && headOfFile[9] == 'E' && headOfFile[10] == 'B' && headOfFile[11] == 'P')
+			std::cout << CYAN << "It's a webp file !" << RESET << std::endl;
+		else
+			std::cout << RED << "It's not a valid image file (.wev or .avi) !" << RESET << std::endl;
+	}
+	else
+		std::cout << RED << "It's not a valid image file !" << RESET << std::endl;
+	
+}
+
--- Scorpion/Scorpion.hpp
+++ Scorpion/Scorpion.hpp
@@ -6,12 +6,14 @@
 /*   By: adnen <adnen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 14:50:21 by adnen             #+#    #+#             */
-/*   Updated: 2026/02/08 14:51:25 by adnen            ###   ########.fr       */
+/*   Updated: 2026/02/08 15:17:18 by adnen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __SCORPION__
 #define __SCORPION__
+
+# include "../Spider/includes.hpp"
 
 class Scorpion
 {
@@ -20,6 +22,8 @@ class Scorpion
         ~Scorpion();
         Scorpion(const Scorpion &src);
         Scorpion &operator=(const Scorpion &src);
+
+		void 	FileAnalysis(const std::string& filename);
 
     private:
         
