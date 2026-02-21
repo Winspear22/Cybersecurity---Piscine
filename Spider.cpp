2a32fcde4b962de251282d1acda5d59308e05af2
Winspear22 <adnen.aloui2@hotmail.com>
2026-02-21 21:25:42 +0100

    arachnide : ex00 terminé

--- Spider.cpp
+++ Spider.cpp
@@ -6,7 +6,7 @@
 /*   By: adnen <adnen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/21 20:12:40 by adnen             #+#    #+#             */
-/*   Updated: 2026/02/21 21:24:34 by adnen            ###   ########.fr       */
+/*   Updated: 2026/02/21 21:25:40 by adnen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,6 @@ Spider::Spider(const Spider &src)
 const Spider &Spider::operator=(const Spider &src)
 {
 	std::cout << "Operatot= de Spider utilisé." << std::endl;
-    // 1. On vérifie si on n'est pas en train de se copier soi-même
     if (this == &src)
         return *this;
     this->_depthNumber = src._depthNumber;
