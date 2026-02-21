6f0a4ce5ce71f223eee2a5c1d8f96677ec9d074d
Winspear22 <adnen.aloui2@hotmail.com>
2026-02-21 23:24:42 +0100

    arachnide : ex01 : spider identation

--- Spider.cpp
+++ Spider.cpp
@@ -6,70 +6,95 @@
 /*   By: adnen <adnen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/21 20:12:40 by adnen             #+#    #+#             */
-/*   Updated: 2026/02/21 23:15:54 by adnen            ###   ########.fr       */
+/*   Updated: 2026/02/21 23:24:10 by adnen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Spider.hpp"
 #include "UseCurl.hpp"
 
-Spider::Spider(void) {
-  std::cout << "Constructeur de Spider utilisé." << std::endl;
-  this->_depthNumber = 5;
-  this->_isRecursive = false;
-  this->_pathOfDownload = "./data/";
+Spider::Spider(void)
+{
+	std::cout << "Constructeur de Spider utilisé." << std::endl;
+	this->_depthNumber = 5;
+	this->_isRecursive = false;
+	this->_pathOfDownload = "./data/";
 }
 
-Spider::Spider(const Spider &src) {
-  std::cout << "Constructeur de recopie Spider utilisé." << std::endl;
-  *this = src;
+Spider::Spider(const Spider &src)
+{
+	std::cout << "Constructeur de recopie Spider utilisé." << std::endl;
+	*this = src;
 }
 
-const Spider &Spider::operator=(const Spider &src) {
-  std::cout << "Operator= de Spider utilisé." << std::endl;
-  if (this == &src)
-    return *this;
-  this->_depthNumber = src._depthNumber;
-  this->_isRecursive = src._isRecursive;
-  this->_pathOfDownload = src._pathOfDownload;
-  this->_url = src._url;
-  return *this;
+const Spider &Spider::operator=(const Spider &src)
+{
+	std::cout << "Operator= de Spider utilisé." << std::endl;
+	if (this == &src)
+		return *this;
+	this->_depthNumber = src._depthNumber;
+	this->_isRecursive = src._isRecursive;
+	this->_pathOfDownload = src._pathOfDownload;
+	this->_url = src._url;
+	return *this;
 }
 
-Spider::~Spider(void) {
-  std::cout << "Destructeur de Spider utilisé." << std::endl;
+Spider::~Spider(void)
+{
+	std::cout << "Destructeur de Spider utilisé." << std::endl;
 }
 
-bool Spider::getIsRecursive(void) { return this->_isRecursive; }
-
-void Spider::setIsRecursive(bool isRecursive) {
-  this->_isRecursive = isRecursive;
+bool Spider::getIsRecursive(void)
+{
+	return this->_isRecursive;
 }
 
-int Spider::getDepthNumber(void) { return this->_depthNumber; }
-
-void Spider::setDepthNumber(int nb) { this->_depthNumber = nb; }
-
-std::string Spider::getPathOfDownload(void) { return this->_pathOfDownload; }
-
-void Spider::setPathOfDownload(std::string path) {
-  this->_pathOfDownload = path;
+void Spider::setIsRecursive(bool isRecursive)
+{
+	this->_isRecursive = isRecursive;
 }
 
-std::string Spider::getUrl(void) { return this->_url; }
+int Spider::getDepthNumber(void)
+{
+	return this->_depthNumber;
+}
 
-void Spider::setUrl(std::string url) { this->_url = url; }
+void Spider::setDepthNumber(int nb)
+{ 
+	this->_depthNumber = nb;
+}
 
-void Spider::run(void) {
-  UseCurl useCurl;
-  std::string html;
+std::string Spider::getPathOfDownload(void)
+{
+	return this->_pathOfDownload;
+}
 
-  html = useCurl.getHtml(this->_url);
-  if (html.empty())
+void Spider::setPathOfDownload(std::string path)
+{
+	this->_pathOfDownload = path;
+}
+
+std::string Spider::getUrl(void)
+{
+	return this->_url;
+}
+
+void Spider::setUrl(std::string url)
+{
+	this->_url = url;
+}
+
+void Spider::run(void)
+{
+	UseCurl useCurl;
+	std::string html;
+
+	html = useCurl.getHtml(this->_url);
+	if (html.empty())
     std::cerr << "Error: la variable HTML est vide." << std::endl;
-  else {
-    std::cout << "Succès ! " << html.size() << " caractères reçus."
-              << std::endl;
-    std::cout << "Aperçu : " << html.substr(0, 100) << "..." << std::endl;
-  }
+	else
+	{
+		std::cout << "Succès ! " << html.size() << " caractères reçus." << std::endl;
+		std::cout << "Aperçu : " << html.substr(0, 100) << "..." << std::endl;
+	}
 }
