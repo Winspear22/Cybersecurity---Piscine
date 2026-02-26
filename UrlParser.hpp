facf17746a746c9cea0e5a09f7b80329643348b8
Winspear22 <adnen.aloui2@hotmail.com>
2026-02-26 17:46:24 +0100

    arachnide : ex03 cas particuliers ok

--- UrlParser.hpp
+++ UrlParser.hpp
@@ -6,7 +6,7 @@
 /*   By: adnen <adnen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 17:28:11 by adnen             #+#    #+#             */
-/*   Updated: 2026/02/26 17:30:43 by adnen            ###   ########.fr       */
+/*   Updated: 2026/02/26 17:46:17 by adnen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,34 @@
 
 #include "includes.hpp"
 
-class UrlParser {
-public:
-  UrlParser(void);
-  UrlParser(const UrlParser &src);
-  const UrlParser &operator=(const UrlParser &src);
-  ~UrlParser(void);
+class UrlParser
+{
+	public:
+		UrlParser(void);
+		UrlParser(const UrlParser &src);
+		const UrlParser &operator=(const UrlParser &src);
+		~UrlParser(void);
 
-  std::string getUrl();
-  std::string getProtocol();
-  std::string getHost();
-  std::string getBasePath();
+		std::string getUrl();
+		std::string getProtocol();
+		std::string getHost();
+		std::string getBasePath();
 
-  void setUrl(std::string src);
-  void setProtocol(std::string src);
-  void setHost(std::string src);
-  void setBasePath(std::string src);
+		void setUrl(std::string src);
+		void setProtocol(std::string src);
+		void setHost(std::string src);
+		void setBasePath(std::string src);
 
-  void parseUrl(std::string src);
-  std::string resolveUrl(std::string src);
+		void parseUrl(std::string src);
+		std::string resolveUrl(std::string src);
 
-private:
-  std::string _url;
-  std::string _protocol;
-  std::string _host;
-  std::string _basePath;
+	private:
+		std::string _url;
+		std::string _protocol;
+		std::string _host;
+		std::string _basePath;
 
-  std::string _sanitizeUrl(std::string url);
+		std::string _sanitizeUrl(std::string url);
 };
 
 #endif
